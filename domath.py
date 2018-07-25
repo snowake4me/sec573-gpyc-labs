9e20ec1990a29af98d54603b3ed99d1e0f98bf4b
Billy Glenn <snowake4me@gmail.com>
2018-07-24 18:40:35 -0700

    domath update

--- domath.py
+++ domath.py
@@ -7,11 +7,7 @@ except:
 num1 = int(input("Enter integer 1 :"))
 num2 = int(input("Enter integer 2 :"))
 
-print (num1)
-print (num2)
-
 print ("Addition:", num1, " + ", num2, " = ", num1+num2)
 print ("Multiplication:", num1, " * ", num2, " = ", num1*num2)
 q = float(num1)/num2
-print (q)
-print ("division:", float(num1), " / ", num2, " = ", "%06.3f" % q)
+print ("division:", float(num1), " / ", num2, " = ", "%06.2f" % q)
